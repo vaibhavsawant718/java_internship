ed627378dbb865970e38c2fe03501f0863101262
vaibhav <vaibhavsawant718@gmail.com>
2025-01-17 16:11:05 +0530

    For Loop Basic

---
+++ src/For_Loop/BasicForLoop.java
@@ -0,0 +1,14 @@
+package For_Loop;
+
+public class BasicForLoop {
+	
+	public static void main(String[] args) 
+	{
+		
+		for (int i = 1; i < 6; i++)
+		{
+			System.out.println("Iteration : "+i);
+		}
+	}
+
+}
