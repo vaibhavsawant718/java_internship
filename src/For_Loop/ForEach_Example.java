8e11ff1d190850c6f056a61aed521560e8813fac
vaibhav <vaibhavsawant718@gmail.com>
2025-01-19 09:27:29 +0530

    For Loop Examples

---
+++ src/For_Loop/ForEach_Example.java
@@ -0,0 +1,13 @@
+package For_Loop;
+
+public class ForEach_Example 
+{
+	public static void main(String[] args) {
+		int arr[]= {11,12,123,343,466};
+		for(int i:arr)
+		{
+			System.out.println(i);
+		}
+		
+	}
+}
