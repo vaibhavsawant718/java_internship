cd82dac28caf0c50eb8e312b104a7cfccc4ab1e7
vaibhav <vaibhavsawant718@gmail.com>
2025-01-20 16:11:44 +0530

    While Loop Examples

--- src/While_Loop/Even_Number.java
+++ src/While_Loop/Even_Number.java
@@ -5,7 +5,7 @@ public class Even_Number
 	public static void main(String[] args) 
 	{	
 		//Even Number
-		//int number=2;
+//		int number=2;
 //		while(number<=10)
 //		{
 //			System.out.println(number);
@@ -13,11 +13,11 @@ public class Even_Number
 //		}
 		//odd Number
 		int number=1;
-		while(number<=10)
+		while(number<=20)
 			{
 				System.out.println(number);
 				number+=2;
 			}
 		}
 	}
-}
+
--- src/While_Loop/Infinite_loopeExample.java
+++ src/While_Loop/Infinite_loopeExample.java
@@ -5,6 +5,9 @@ public class Infinite_loopeExample
 
 		public static void main(String[] args) 
 		{
-			
+			while (true) {
+				System.out.println("Hello vaibhav");
+				
+			}
 		}
 }
